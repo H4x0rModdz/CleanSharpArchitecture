e6f6a58ce245e48c2b29ed213e5598e6e46b6d29
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-02-12 20:36:35 -0300

    Add Base Entities And Add EntityStatus Enum

--- CleanSharpArchitecture.Domain/Entities/BaseEntity.cs
+++ CleanSharpArchitecture.Domain/Entities/BaseEntity.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using CleanSharpArchitecture.Domain.Enums;
 
 namespace CleanSharpArchitecture.Domain.Entities
 {
     public abstract class BaseEntity
     {
+
         public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; set; }
+        public EntityStatus Status { get; set; } = EntityStatus.Active;
     }
 }
