9709b3d25930cd7308090d55b0313e9d69d4c38f
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-02-12 20:36:52 -0300

    Remove Old Files

--- CleanSharpArchitecture.Infrastructure/Services/BlobSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CleanSharpArchitecture.Infrastructure.Interfaces.Services
-{
-    public class BlobSettings
-    {
-        public string ConnectionString { get; set; }
-        public string ContainerName { get; set; }
-    }
-}
